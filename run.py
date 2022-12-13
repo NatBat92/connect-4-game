375baab6ecb14ab466894a1cae58b0cda3bda1f0
NatBat92 <natalie.neale1992@outlook.com>
2022-12-13 12:11:43 +0000

    Add game turns and check for win functions

--- run.py
+++ run.py
@@ -35,3 +35,53 @@ class GameBoard():
             print("\n")
 
         print(f"{'-' * 35}\n")
+
+    def turns(self):
+        """
+        creates the turns and the symbols of each player
+        """
+        players = ['#', '@']
+        return players[self.turn % 2]
+
+    def player_go(self, column):
+        """
+        determines the go has been completed by searching the column
+        that was chosen for a change
+        """
+        # Search from the bottom of the column up
+        for k in range(BOARD_ROWS-1, -1, -1):
+            if self.board[k][column] == ' ':
+                self.board[k][column] = self.turns()
+                self.last_move = [k, column]
+
+                self.turn += 1
+                return True
+
+        return False
+
+    def in_grid(self, j, i):
+        """
+        checks if the players turn was a valid move
+        inside the parameters of the game boad
+        """
+        return (j >= 0 and j < BOARD_ROWS and i >= 0 and i < BOARD_COLUMNS)
+
+    def check_for_win(self):
+        """
+        checks the game board for 4 in a row to determine the end of the game
+        """
+        last_row = self.last_move[0]
+        last_column = self.last_move[1]
+        last_symbol = self.board[last_row][last_column]
+
+        # [i, j] direction, matching symbol count
+        win_directions = [
+            [[-1, 0], 0, True],
+            [[1, 0], 0, True],
+            [[0, -1], 0, True],
+            [[0, 1], 0, True],
+            [[-1, -1], 0, True],
+            [[1, 1], 0, True],
+            [[-1, 1], 0, True],
+            [[1, -1], 0, True],
+        ]
